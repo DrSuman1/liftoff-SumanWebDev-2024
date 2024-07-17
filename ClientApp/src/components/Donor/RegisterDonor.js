14dc8fd0da4d68a553c1e608ee724aa55be71b64
Suman <dr.suman1jan@gmail.com>
2024-07-17 10:58:49 -0500

    Added style.css and fixed style of Donor registration form.

--- ClientApp/src/components/Donor/RegisterDonor.js
+++ ClientApp/src/components/Donor/RegisterDonor.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import states from '../StatesList';
+import './style.css';
+
 
 export class RegisterDonor extends Component {
     static displayName = RegisterDonor.name;
@@ -92,81 +94,256 @@ export class RegisterDonor extends Component {
 
     render() {
         return (
- 
-            <div>
+            <div className="registration-form">
                 <form onSubmit={this.handleSubmit}>
                     <h2>Register Donor</h2>
 
-                    <p>
-                        <label>First Name: </label>
-                        <input id="firstName" type="text" placeholder="Enter First Name" value={this.state.firstName} onChange={this.handleChange} required/>
-                    </p>
-                    <p>
-                        <label>Last Name: </label>
-                        <input id="lastName" type="text" placeholder="Enter Last Name" value={this.state.lastName} onChange={this.handleChange} required/>
-                    </p>
-                    <p>
-                        <label>Gender: </label>
-                        <select id="gender" value={this.state.gender} onChange={this.handleChange} required>
+                    <div className="form-group">
+                        <label htmlFor="firstName">First Name:</label>
+                        <input
+                            id="firstName"
+                            type="text"
+                            placeholder="Enter First Name"
+                            value={this.state.firstName}
+                            onChange={this.handleChange}
+                            required
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="lastName">Last Name:</label>
+                        <input
+                            id="lastName"
+                            type="text"
+                            placeholder="Enter Last Name"
+                            value={this.state.lastName}
+                            onChange={this.handleChange}
+                            required
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="gender">Gender:</label>
+                        <select
+                            id="gender"
+                            value={this.state.gender}
+                            onChange={this.handleChange}
+                            required
+                        >
                             <option value="">Select Gender</option>
                             <option value="male">Male</option>
                             <option value="female">Female</option>
                             <option value="other">Other</option>
                         </select>
-                    </p>
-                    <p>
-                        <label>Date of Birth: </label>
-                        <input id="dateOfBirth" type="text" placeholder="MM/DD/YYYY" value={this.state.dateOfBirth} onChange={this.handleChange} required/>
-                    </p>
-                    <p>
-                        <label>Blood Type: </label>
-                        <select id="bloodType" value={this.state.bloodType} onChange={this.handleChange} required>
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="dateOfBirth">Date of Birth:</label>
+                        <input
+                            id="dateOfBirth"
+                            type="text"
+                            placeholder="MM/DD/YYYY"
+                            value={this.state.dateOfBirth}
+                            onChange={this.handleChange}
+                            required
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="bloodType">Blood Type:</label>
+                        <select
+                            id="bloodType"
+                            value={this.state.bloodType}
+                            onChange={this.handleChange}
+                            required
+                        >
                             <option value="">Select Blood Type</option>
                             <option value="A">A</option>
                             <option value="B">B</option>
                             <option value="AB">AB</option>
                             <option value="O">O</option>
                         </select>
-                        <select id="rhFactor" value={this.state.rhFactor} onChange={this.handleChange} required>
-                        <option value="">Select +/-</option>
+
+                        <select
+                            id="rhFactor"
+                            value={this.state.rhFactor}
+                            onChange={this.handleChange}
+                            required
+                        >
+                            <option value="">Select +/-</option>
                             <option value="+">+</option>
                             <option value="-">-</option>
                         </select>
-                    </p>
-                    <p>
-                        <label>Address </label>
-                        <input id="address" type="text" placeholder="Enter Address" value={this.state.address} onChange={this.handleChange} required/>
-                    </p>
-                    <p>
-                        <label>City </label>
-                        <input id="city" type="text" placeholder="Enter City" value={this.state.city} onChange={this.handleChange} required/>
-                    </p>
-                    <p>
-                        <label>State</label>
-                        <select id="state" value={this.state.state} onChange={this.handleChange} required>
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="address">Address:</label>
+                        <input
+                            id="address"
+                            type="text"
+                            placeholder="Enter Address"
+                            value={this.state.address}
+                            onChange={this.handleChange}
+                            required
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="city">City:</label>
+                        <input
+                            id="city"
+                            type="text"
+                            placeholder="Enter City"
+                            value={this.state.city}
+                            onChange={this.handleChange}
+                            required
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="state">State:</label>
+                        <select
+                            id="state"
+                            value={this.state.state}
+                            onChange={this.handleChange}
+                            required
+                        >
                             <option value="">Select a state</option>
-                            {states.map((state, index) => (<option key={index} value={state}>{state}</option>))}
+                            {states.map((state, index) => (
+                                <option key={index} value={state}>
+                                    {state}
+                                </option>
+                            ))}
                         </select>
-                    </p>
-                    <p>
-                        <label>E-mail </label>
-                        <input id="email" type="email" placeholder="Enter E-Mail" value={this.state.email} onChange={this.handleChange} required />
-                    </p>
-                    <p>
-                        <label>Phone Number </label>
-                        <input id="phone" type="tel" placeholder="(###)-###-###" value={this.state.phone} onChange={this.handleChange} required />
-                    </p>
-                    <p>
-                        <label>Password </label>
-                        <input id="password" type="password" placeholder="Enter Password" value={this.state.password} onChange={this.handleChange} required/>
-                    </p>
-                    <p>
-                        <label>Confirm Password </label>
-                        <input id="confirmPassword" type="password" placeholder="Confirm Password" value={this.state.confirmPassword} onChange={this.handleChange} required/>
-                    </p>
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="email">E-mail:</label>
+                        <input
+                            id="email"
+                            type="email"
+                            placeholder="Enter E-Mail"
+                            value={this.state.email}
+                            onChange={this.handleChange}
+                            required
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="phone">Phone Number:</label>
+                        <input
+                            id="phone"
+                            type="tel"
+                            placeholder="(###)-###-####"
+                            value={this.state.phone}
+                            onChange={this.handleChange}
+                            required
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="password">Password:</label>
+                        <input
+                            id="password"
+                            type="password"
+                            placeholder="Enter Password"
+                            value={this.state.password}
+                            onChange={this.handleChange}
+                            required
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="confirmPassword">Confirm Password:</label>
+                        <input
+                            id="confirmPassword"
+                            type="password"
+                            placeholder="Confirm Password"
+                            value={this.state.confirmPassword}
+                            onChange={this.handleChange}
+                            required
+                        />
+                    </div>
+
                     <button type="submit">Register</button>
                 </form>
             </div>
+
+ 
+            //<div>
+            //    <form onSubmit={this.handleSubmit}>
+            //        <h2>Register Donor</h2>
+
+            //        <p>
+            //            <label>First Name: </label>
+            //            <input id="firstName" type="text" placeholder="Enter First Name" value={this.state.firstName} onChange={this.handleChange} required/>
+            //        </p>
+            //        <p>
+            //            <label>Last Name: </label>
+            //            <input id="lastName" type="text" placeholder="Enter Last Name" value={this.state.lastName} onChange={this.handleChange} required/>
+            //        </p>
+            //        <p>
+            //            <label>Gender: </label>
+            //            <select id="gender" value={this.state.gender} onChange={this.handleChange} required>
+            //                <option value="">Select Gender</option>
+            //                <option value="male">Male</option>
+            //                <option value="female">Female</option>
+            //                <option value="other">Other</option>
+            //            </select>
+            //        </p>
+            //        <p>
+            //            <label>Date of Birth: </label>
+            //            <input id="dateOfBirth" type="text" placeholder="MM/DD/YYYY" value={this.state.dateOfBirth} onChange={this.handleChange} required/>
+            //        </p>
+            //        <p>
+            //            <label>Blood Type: </label>
+            //            <select id="bloodType" value={this.state.bloodType} onChange={this.handleChange} required>
+            //                <option value="">Select Blood Type</option>
+            //                <option value="A">A</option>
+            //                <option value="B">B</option>
+            //                <option value="AB">AB</option>
+            //                <option value="O">O</option>
+            //            </select>
+            //            <select id="rhFactor" value={this.state.rhFactor} onChange={this.handleChange} required>
+            //            <option value="">Select +/-</option>
+            //                <option value="+">+</option>
+            //                <option value="-">-</option>
+            //            </select>
+            //        </p>
+            //        <p>
+            //            <label>Address </label>
+            //            <input id="address" type="text" placeholder="Enter Address" value={this.state.address} onChange={this.handleChange} required/>
+            //        </p>
+            //        <p>
+            //            <label>City </label>
+            //            <input id="city" type="text" placeholder="Enter City" value={this.state.city} onChange={this.handleChange} required/>
+            //        </p>
+            //        <p>
+            //            <label>State</label>
+            //            <select id="state" value={this.state.state} onChange={this.handleChange} required>
+            //                <option value="">Select a state</option>
+            //                {states.map((state, index) => (<option key={index} value={state}>{state}</option>))}
+            //            </select>
+            //        </p>
+            //        <p>
+            //            <label>E-mail </label>
+            //            <input id="email" type="email" placeholder="Enter E-Mail" value={this.state.email} onChange={this.handleChange} required />
+            //        </p>
+            //        <p>
+            //            <label>Phone Number </label>
+            //            <input id="phone" type="tel" placeholder="(###)-###-###" value={this.state.phone} onChange={this.handleChange} required />
+            //        </p>
+            //        <p>
+            //            <label>Password </label>
+            //            <input id="password" type="password" placeholder="Enter Password" value={this.state.password} onChange={this.handleChange} required/>
+            //        </p>
+            //        <p>
+            //            <label>Confirm Password </label>
+            //            <input id="confirmPassword" type="password" placeholder="Confirm Password" value={this.state.confirmPassword} onChange={this.handleChange} required/>
+            //        </p>
+            //        <button type="submit">Register</button>
+            //    </form>
+            //</div>
         );
     }
 }
